38e375a821c8969f957ce7515addf40667ec3bad
lebatsnok <lebatsnok@gmail.com>
2021-11-14 21:09:58 +0200

    added textsize arg

--- R/kaart.R
+++ R/kaart.R
@@ -12,21 +12,21 @@
 #' @return a ggplot
 #' @details tba
 #' @export
-kaart <- function(df, fill, labels, title="", legend.title = "", annotation="kaart: Maa-amet, 2021"){
+kaart <- function(df, fill, labels, title="", legend.title = "", textsize = 7, annotation="kaart: Maa-amet, 2021"){
   #chk
   useless <- ""
   FILL <- deparse(substitute(fill))
   LABS <- deparse(substitute(labels))
-  ggplot(df, aes(fill=.data[[FILL]]))+
-    geom_map(aes(map_id=maakond), map=maakonnad, color="black")+
-    expand_limits(x=maakonnad$long, y=maakonnad$lat)+
-    coord_fixed()+
-    geom_text(aes(label=.data[[LABS]],x=x, y=y), data=df)+
-    theme(axis.title=element_blank(), axis.text=element_blank(),
-          line=element_blank(), panel.background=element_blank())+
-    ggtitle(title)+
-    scale_fill_brewer(legend.title) +
-    annotate("text", x = 700000, y = 6370000, label = annotation, size=3, color="grey")
+  ggplot2::ggplot(df, ggplot2::aes(fill=.data[[FILL]]))+
+    ggplot2::geom_map(ggplot2::aes(map_id=maakond), map=maakonnad, color="black")+
+    ggplot2::expand_limits(x=maakonnad$long, y=maakonnad$lat)+
+    ggplot2::coord_fixed()+
+    ggplot2::geom_text(ggplot2::aes(label=.data[[LABS]],x=x, y=y), data=df, size=textsize)+
+    ggplot2::theme(axis.title=element_blank(), axis.text=element_blank(),
+          line=ggplot2::element_blank(), panel.background=ggplot2::element_blank())+
+    ggplot2::ggtitle(title)+
+    ggplot2::scale_fill_brewer(legend.title) +
+    ggplot2::annotate("text", x = 700000, y = 6370000, label = annotation, size=3, color="grey")
 }
 
 
@@ -44,25 +44,27 @@ kaart <- function(df, fill, labels, title="", legend.title = "", annotation="kaa
 #' @return a ggplot
 #' @details tba
 #' @export
-kaart2 <- function(df, fill, labels, title="", legend.title = "", annotation="kaart: Maa-amet, 2021"){
+kaart2 <- function(df, fill, labels, title="", legend.title = "", textsize=7, annotation="kaart: Maa-amet, 2021"){
   FILL <- deparse(substitute(fill))
   LABS <- deparse(substitute(labels))
-  ggplot(df, aes(fill=.data[[FILL]]))+
-    geom_map(aes(map_id=maakond), map=maakonnad, color="grey")+
-    geom_map(aes(map_id=maakond), map=tlntrt, color="black")+
-    expand_limits(x=maakonnad$long, y=maakonnad$lat)+
-    coord_fixed()+
-    geom_text(aes(label=.data[[LABS]],x=x-5000, y=y+5000), data=subset(df, maakond%in%c("Tallinn", "Tartu")),
-              hjust=1, vjust=0)+
-    geom_text(aes(label=.data[[LABS]],x=x-1000, y=y), data=subset(df, !maakond%in%c("Tallinn", "Tartu") &
-                                                                    !maakond %in% c("Harju maakond", "Tartu maakond")), hjust=1, vjust=0)+
-    geom_text(aes(label=.data[[LABS]],x=x+21000, y=y-1000), data=subset(df,
-                                                                        maakond %in% c("Harju maakond", "Tartu maakond")), hjust=1, vjust=0)+
-    theme(axis.title=element_blank(), axis.text=element_blank(),line=element_blank(),
-          panel.background=element_blank())+
-    ggtitle(title)+
-    scale_fill_brewer(legend.title) +
-    annotate("text", x = 700000, y = 6370000, label = annotation, size=3, color="grey")
+  ggplot2::ggplot(df, ggplot2::aes(fill=.data[[FILL]]))+
+    ggplot2::geom_map(ggplot2::aes(map_id=maakond), map=maakonnad, color="grey")+
+    ggplot2::geom_map(ggplot2::aes(map_id=maakond), map=tlntrt, color="black")+
+    ggplot2::expand_limits(x=maakonnad$long, y=maakonnad$lat)+
+    ggplot2::coord_fixed()+
+    ggplot2::geom_text(ggplot2::aes(label=.data[[LABS]],x=x-5000, y=y+5000), data=subset(df, maakond%in%c("Tallinn", "Tartu")),
+              hjust=1, vjust=0, size=textsize)+
+    ggplot2::geom_text(ggplot2::aes(label=.data[[LABS]],x=x-1000, y=y), data=subset(df, !maakond%in%c("Tallinn", "Tartu") &
+                                                                    !maakond %in% c("Harju maakond", "Tartu maakond")), hjust=1, vjust=0,
+                       size=textsize)+
+    ggplot2::geom_text(ggplot2::aes(label=.data[[LABS]],x=x+21000, y=y-1000), data=subset(df,
+                                                                        maakond %in% c("Harju maakond", "Tartu maakond")), hjust=1, vjust=0,
+                       size=textsize)+
+    ggplot2::theme(axis.title=element_blank(), axis.text=ggplot2::element_blank(),line=ggplot2::element_blank(),
+          panel.background=ggplot2::element_blank())+
+    ggplot2::ggtitle(title)+
+    ggplot2::scale_fill_brewer(legend.title) +
+    ggplot2::annotate("text", x = 700000, y = 6370000, label = annotation, size=3, color="grey")
 }
 
 
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # kaart
 
-Andmete esitamine Eesti maakondade kaardil. Selles paketis on kaks peamist funktsiooni: `kaart` ja `kaart2`: esimene näitab pildil 15 maakonda, teine lisaks veel Tallinnat ja Tartut. Kaardi joonistamiseks kasutatakse `ggplot2` - see on ka ainuke eeldatav pakett, milleta läbi ei saa. Suurem osa koodist pärineb [Indrek Seppo postitusest](https://analyticsestonia.wordpress.com/2013/11/21/kuidas-teha-eesti-maakaarti-r-is/) (kes lahkelt lubas seda kasutada), seda - või siis siinsete funktsioonide lähtekoodi - tasub uurida, kui tahad teistsugust kaarti, näiteks kujutada kaardil valdu või teisi linnu lisaks kahele suuremale. Siinses paketis on kaardi joonistamine tehtud mugavamaks: Eesti kaart (koos maakondade jms piirjoontega) on komplektis, seda ei ole vaja eraldi alla tõmmata ega kartograafiatarkvara abil eeltöödelda.
+Andmete esitamine Eesti maakondade kaardil. Selles paketis on kaks peamist funktsiooni: `kaart` ja `kaart2`: esimene näitab pildil 15 maakonda, teine lisaks veel Tallinna ja Tartut. Kaardi joonistamiseks kasutatakse `ggplot2` - see on ka ainuke eeldatav pakett, milleta läbi ei saa. Algne kood pärineb [Indrek Seppo blogipostitusest](https://analyticsestonia.wordpress.com/2013/11/21/kuidas-teha-eesti-maakaarti-r-is/) (kes lahkelt lubas seda kasutada), seda - või siis siinsete funktsioonide lähtekoodi - tasub uurida, kui tahad teistsugust kaarti, näiteks kujutada kaardil valdu või teisi linnu lisaks kahele suuremale. Siinses paketis on kaardi joonistamine tehtud mugavamaks: Eesti kaart (koos maakondade jms piirjoontega) on komplektis, seda ei ole vaja eraldi alla tõmmata ega kartograafiatarkvara abil eeltöödelda.
 
 Kõige lihtsam on alustada kaasapandud andmestikust `osalus`: seal on kaks tunnust, mida järgnevas näites on kasutatud:
 
-* `osalus` - arvtunnus, mis lisatakse sellisel kujul kaardile õigesse kohta 
+* `osalus` - arvtunnus, mis lisatakse sellisel kujul kaardile õigesse kohta (kuid see võib olla ka tekstiline tunnus, nt maakondade nimed; arvtunnuste puhul tuleks jälgida ümardamist)
 * `vahemik` - järjestustunnus v faktor - selle alusel värvitakse maakonnad erineva värviga
 
 Samale andmestikule võib lisada oma andmeid või kirjutada olemasolevate tulpade asemele. Ainsad tulbad, mida ei maksaks eriti muuta, on `maakond` (maakonna nimi peab jääma samale kujule), `x` ja `y` (maakondade tsentroidide st keskpunktide koordinaadid).
@@ -15,4 +15,4 @@ library(kaart)
 kaart(osalus[1:15,], fill=vahemik, labels=osalus, title="KOV valimistest 2013. osavõtt maakondade kaupa",  legend.title="Osalusmäär")
 ```
 
-![](kaart.png)
+![](kaart.png)  
